e5ce4a4b2dabf09759279bb06640ca67ebc2dc47
m01x <fortiz.informatica@gmail.com>
2025-01-27 22:43:23 -0300

    classes y data

--- src/classes/Hero.ts
+++ src/classes/Hero.ts
@@ -1,10 +1,14 @@
-class Hero {
+import powers from '../data/powers';
+
+export class Hero {
 
     constructor(
         public name: string,
         public powerId: number,
         public age:number
     ){}
-}
 
-export Hero;
+    get power(): string{//return string
+        return powers.find( power => power.id === this.powerId )?.desc || 'not found';
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
+import { Hero } from "./classes/Hero";
+import powers from "./data/powers";
 
 
-console.log('Hola Mundo!');
+console.log('index started!');
 
+const ironman = new Hero('Ironman', 2, 35);
 
+console.log(ironman.power);
 
 
 
